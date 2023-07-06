e836b2f6c3e27d0bc580d30cd959c297af6b3f8f
Aruzhan Sabitkyzy <arusabitkyzy03@mail.ru>
2023-07-06 13:31:36 +0500

    basic game logic is done

--- app/game.tsx
+++
@@ -1,6 +0,0 @@
-class Game {
-
-   constructor() {
-
-   }
-}
---
+++ gameManager/Player.tsx
@@ -0,0 +1,25 @@
+class Player {
+    username:string
+    tuzdyq:number
+    isGoing:boolean 
+    score: number
+    constructor(username:string) {
+        this.username = username;
+        this.score = 0;
+        this.isGoing = false;
+        this.setScore = this.setScore.bind(this);
+        this.tuzdyq = -1;
+    }
+
+    setScore(score:number) {
+         if(typeof score === 'number')
+            this.score+= score
+    }
+
+    getScore() {
+        return this.score
+    }
+    isTuzdyq(index:number) {
+         return index == this.tuzdyq   
+    }
+}
